80779c55ae4a0dfb9d32f602cb9d9be61c2a4417
Idumii <vincent.dievart.8@gmail.com>
2025-04-10 12:23:09 +0200

    Updates env file path resolution
    Changes the env file inclusion to use an absolute path with `__DIR__` for improved file location accuracy and portability.

--- api/enregistrer_partie.php
+++ api/enregistrer_partie.php
@@ -7,7 +7,7 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 
-require_once("env.php");
+require_once(__DIR__ . "/.env.php");
 
 // Connexion BDD
 try {
